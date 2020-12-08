83a6d6dc5efe58aad4e479d5af634dbe885bb123
sauriomac <40914504+sauriomac@users.noreply.github.com>
2020-12-08 21:47:53 +0200

    prueba de express2

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-const express = require('express');
-const app = express();
-const port = 3000
-const morgan = require('morgan');
-const cors = require('cors');
-const path = require('path');
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
 
-//middleware
+const app = express();
+
+// Middleware
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
-app.get('/', function (req, res) {
-    res.send('hello con babel');
-  });
-  
-  // Middleware para Vue.js router modo history
-  const history = require('connect-history-api-fallback');
-  app.use(history());
-  app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-// app.set('puerto', process.env.PORT || 3000);
-app.listen(port, ()=>{
-console.log('en escucha ahora en :  ', port)
-})
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('puerto', process.env.PORT || 3000);
+app.listen(app.get('puerto'), () => {
+  console.log('Example app listening on port'+ app.get('puerto'));
+});
